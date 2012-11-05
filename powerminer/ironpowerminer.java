4ceddeada2c463fbc7eddcf6f06a0a93566eeac8
bautista4 <bautidastud@gmail.com>
2012-11-04 21:30:07 -0600

    improved power miner. huge thanks to shantred!

--- powerminer/ironpowerminer.java
+++ powerminer/ironpowerminer.java
@@ -23,10 +23,10 @@ import org.powerbot.game.api.wrappers.node.Item;
 import org.powerbot.game.api.wrappers.node.SceneObject;
 
 @Manifest(authors = { "bautista4" }, name = "B4PowerMiner", description = "Powermines iron", version = 0.1)
-public class ironpowerminer extends ActiveScript implements PaintListener{
+public class ironpowerminer extends ActiveScript implements PaintListener {
 
-	public static final int ROCK_ID = 11954;
-	public static final int MINNED_ID = 11555;
+	int[] ROCK_ID = { 11954, 11955, 11956 };
+	int[] MINNED_ID = { 11555, 11556, 11556 };
 	public static final int IRON_ID = 440;
 
 	Tree jobs = null;
@@ -34,16 +34,16 @@ public class ironpowerminer extends ActiveScript implements PaintListener{
 	public int loop() {
 		if (jobs == null) {
 			jobs = new Tree(new Node[] { new Mining(), new Dropping() });
-			if (jobs != null) {
-				Node job = jobs.state();
-				if (job != null) {
-					jobs.set(job);
-					getContainer().submit(job);
-					job.join();
-				}
-			}
 		}
-		return 25;
+
+		final Node job = jobs.state();
+		if (job != null) {
+			jobs.set(job);
+			getContainer().submit(job);
+			job.join();
+			return 0;
+		}
+		return Random.nextInt(200, 300);
 	}
 
 	public class Mining extends Node {
@@ -54,29 +54,33 @@ public class ironpowerminer extends ActiveScript implements PaintListener{
 		}
 
 		public void execute() {
+			System.out.println("Executing Mining");
 			SceneObject rock = SceneEntities.getNearest(ROCK_ID);
 			if (rock != null && rock.isOnScreen()
 					&& Players.getLocal().getAnimation() == -1
 					&& !Players.getLocal().isMoving())
-				;
+			// There used to be a semicolon here. No semicolons in if statements
+
 			{
-				System.out.println("Minning");
+				System.out.println("Rock found, on screen");
 				Mouse.click(rock.getCentralPoint(), true);
 				Task.sleep(200, 500);
-			}
+				System.out.println("Done sleeping. look for new rock");
 
-			if (rock != null && !rock.isOnScreen()
+			} else if (rock != null && !rock.isOnScreen()
 					&& Players.getLocal().getAnimation() == -1
 					&& !Players.getLocal().isMoving())
-				;
+			// Used to be a semicolon here, too
 			{
+				System.out.println("Turning screen");
 				Camera.turnTo(rock);
-				Task.sleep(500, 800);
-				System.out.println("Minning1");
+				System.out.println("Camera turned, mining.");
 				Mouse.click(rock.getCentralPoint(), true);
 				Task.sleep(400, 600);
+			} else {
+				System.out.println("No case found");
 			}
-
+			System.out.println("Out of IF");
 		}
 	}
 
@@ -88,50 +92,50 @@ public class ironpowerminer extends ActiveScript implements PaintListener{
 		}
 
 		public void execute() {
-			System.out.println("Dropping");
+			System.out.println("Attempting to drop");
 			for (Item i : Inventory.getItems()) {
-                if (i.getId() == IRON_ID) {
-                        i.getWidgetChild().interact("Drop");
-                        sleep(Random.nextInt(200, 500));
-                }
-        }
+				if (i.getId() == IRON_ID) {
+					i.getWidgetChild().interact("Drop");
+					sleep(Random.nextInt(200, 500));
+				}
+			}
 		}
 
 	}
-   
-    private Image getImage(String url) {
-        try {
-            return ImageIO.read(new URL(url));
-        } catch(IOException e) {
-            return null;
-        }
-    }
 
-    private final Color color1 = new Color(0, 0, 0);
-    private final Color color2 = new Color(204, 0, 0);
+	private Image getImage(String url) {
+		try {
+			return ImageIO.read(new URL(url));
+		} catch (IOException e) {
+			return null;
+		}
+	}
 
-    private final BasicStroke stroke1 = new BasicStroke(4);
+	private final Color color1 = new Color(0, 0, 0);
+	private final Color color2 = new Color(204, 0, 0);
 
-    private final Font font1 = new Font("Arial", 1, 20);
+	private final BasicStroke stroke1 = new BasicStroke(4);
 
-    private final Image img1 = getImage("http://runescape.neoseeker.com/w/i/runescape/7/76/Mining_Cape_(t).gif");
-    private final Image img2 = getImage("https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ4tJsNJo6puEKul6HQCBRlpUyt50YFlwkuqz7tnA29Mn-3fa3NQA");
+	private final Font font1 = new Font("Arial", 1, 20);
 
-    public void onRepaint(Graphics g1) {
-        Graphics2D g = (Graphics2D)g1;
-        g.setColor(color1);
-        g.fillRoundRect(3, 342, 513, 133, 16, 16);
-        g.setColor(color2);
-        g.setStroke(stroke1);
-        g.drawRoundRect(3, 342, 513, 133, 16, 16);
-        g.drawImage(img1, 341, 346, null);
-        g.drawImage(img1, 129, 346, null);
-        g.setFont(font1);
-        g.drawString("B4IronPowerMiner", 170, 374);
-        g.drawString("Author: bautista", 178, 399);
-        g.drawString("Enjoy 99 mining! :D", 161, 441);
-        g.drawImage(img2, 357, 417, null);
-        g.drawImage(img2, 117, 417, null);
-    }
-    
+	private final Image img1 = getImage("http://runescape.neoseeker.com/w/i/runescape/7/76/Mining_Cape_(t).gif");
+	private final Image img2 = getImage("https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ4tJsNJo6puEKul6HQCBRlpUyt50YFlwkuqz7tnA29Mn-3fa3NQA");
+
+	public void onRepaint(Graphics g1) {
+		Graphics2D g = (Graphics2D) g1;
+		g.setColor(color1);
+		g.fillRoundRect(3, 342, 513, 133, 16, 16);
+		g.setColor(color2);
+		g.setStroke(stroke1);
+		g.drawRoundRect(3, 342, 513, 133, 16, 16);
+		g.drawImage(img1, 341, 346, null);
+		g.drawImage(img1, 129, 346, null);
+		g.setFont(font1);
+		g.drawString("B4IronPowerMiner", 170, 374);
+		g.drawString("Author: bautista", 178, 399);
+		g.drawString("Enjoy 99 mining! :D", 161, 441);
+		g.drawImage(img2, 357, 417, null);
+		g.drawImage(img2, 117, 417, null);
+	}
+
 }
